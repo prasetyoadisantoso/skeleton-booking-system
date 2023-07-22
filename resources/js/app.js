a09fc8594d17a95d9038fd936a4a00b507eb8d81
satriyadi <satriyadi3001@gmail.com>
2023-07-22 09:21:34 +0800

    add inertia and vuejs baru

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,8 +3,8 @@ import { createInertiaApp } from '@inertiajs/vue3'
 
 createInertiaApp({
     resolve: name => {
-        const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
-        return pages[`./Pages/${ name }.vue`]
+        const pages = import.meta.glob('../views/template/inertia/**/*.vue', { eager: true })
+        return pages[`../views/template/inertia/layout/${ name }.vue`]
     },
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
